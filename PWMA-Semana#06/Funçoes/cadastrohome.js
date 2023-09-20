f55f56737ffb581ccdde01116f1f98c4be7b242b
Erick Duarte Bernat <138524660+ErickBernat@users.noreply.github.com>
2023-09-20 09:03:23 -0300

    Update cadastrohome.js

--- PWMA-Semana#06/Funçoes/cadastrohome.js
+++ PWMA-Semana#06/Funçoes/cadastrohome.js
@@ -2,7 +2,7 @@ function cadastrohome() {
     var nome = document.getElementById('campnome').value;
     var email = document.getElementById('campemail').value;
 
-    if (nome == false && email == false) {
+    if (nome == false || email == false) {
         alert('Voce se cadastrou incorretamente, tente novamente!');
     } else {
         alert('Voce se cadastrou com sucesso!!');
